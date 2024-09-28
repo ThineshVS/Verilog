fa8d35a6311ac2d0ae8c4354f7c810b6494b00f6
Thinesh V S <61322573+ThineshVS@users.noreply.github.com>
2024-09-28 23:09:21 +0530

    Create tFlipFlopDesign.sv

---
+++ Flipflops/tFlipFlopDesign.sv
@@ -0,0 +1,23 @@
+module tflipflop(input t, input clk, input rst, output reg q, output reg qbar);
+  //srFlipFlop get inputs {s,r}, reset,clk Output q and q'
+  always@(posedge clk)
+  //Positive edge triggered flipflop
+    if(!rst) q <= 0;
+  //This is neg of rst (0)' = 1 then q is 0.
+     else
+      begin 
+        case({t})
+          1'b0: q <= q;
+          1'b1: q <= ~q;
+        endcase 
+        assign qbar = ~q;
+        
+        // | T | Q (Next State) | Q' (Complement) |
+        // |---|-----------------|------------------|
+        // | 0 |      Q_prev     |      Q'_prev     |
+        // | 1 |      Q'         |        Q         |
+
+        #1 $display("Design output t= %b q= %b q=%b reset=%b", t,q,qbar,rst);
+      end
+
+endmodule
