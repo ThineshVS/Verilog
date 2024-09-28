af66e42a2af1cfae3991396c7e5e6af5719312c9
Thinesh V S <61322573+ThineshVS@users.noreply.github.com>
2024-09-28 23:49:34 +0530

    jkFlipFlopTestBench.sv: Took care of nits

--- Flipflops/jkFlipFlopTestBench.sv
+++ Flipflops/jkFlipFlopTestBench.sv
@@ -1,21 +1,21 @@
 module testb();
   reg clk, rst;
-  reg s,r;
+  reg j,k;
   wire q,qbar;
 //jkFlipFlop get inputs {s,r}, reset,clk Output q and q'
   task valueparse(bit [1:0] ip);
     //we are creating a task since we are going to change the value of {s.r} constantly.
     //we are using task since we are using timing constructs
     @(posedge clk);//while the clk is at positive edge(0-to-1).
-    {s,r} = ip;//This line is used to change the value of s&r as we instantiate the clock
-    #1 $display("s= %b r= %b --> q= %b qbar= %b",s,r,q,qbar);//display the values for every cycle.
+    {j,k} = ip;//This line is used to change the value of s&r as we instantiate the clock
+    #1 $display("j= %b k= %b --> q= %b qbar= %b",j,k,q,qbar);//display the values for every cycle.
   endtask
-  jkflipflop jkff(.clk(clk), .s(s), .r(r),.rst(rst),.q(q),.qbar(qbar));//creating design instance.
+  jkflipflop jkff(.clk(clk), .j(j), .k(k),.rst(rst),.q(q),.qbar(qbar));//creating design instance.
   always #2 clk = ~clk; //negating clk value every 2ns
   initial begin
     clk = 0;rst = 0;//initiating clk values
     #3 rst=1;
-    //setting reset equals 1 and doing some permutation&combinations of the inout values {s,r}.
+    //setting reset equals 1 and doing some permutation&combinations of the inout values {j,k}.
     valueparse(2'b00);
     valueparse(2'b01);
     valueparse(2'b10);
